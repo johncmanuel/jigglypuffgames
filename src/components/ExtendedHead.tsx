80b7f22080a27f8ad2edaf2e7f4a3243d5ee4358
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-05-27 10:35:00 -0700

    modify head component

--- src/components/ExtendedHead.tsx
+++ src/components/ExtendedHead.tsx
@@ -2,21 +2,19 @@ import Head from "next/head";
 
 type ExtendedHeadProps = {
 	title: string;
-	description: string;
 };
 
 export const ExtendedHead: React.FC<ExtendedHeadProps> = ({
 	title,
-	description,
 }) => {
 	return (
 		<Head>
 			<title>{title}</title>
-			<meta name="description" content={description} />
 			<meta
 				name="viewport"
 				content="width=device-width, initial-scale=1"
 			/>
+			<meta name="color-scheme" content="light dark" />
 			<link rel="icon" href="/favicon.ico" />
 		</Head>
 	);
