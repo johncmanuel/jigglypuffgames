1bd48e937581008111235959b104ea75dd93dc6b
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-06-16 22:30:01 -0700

    wip

--- src/pages/whack-a-puff/index.tsx
+++ src/pages/whack-a-puff/index.tsx
@@ -3,8 +3,24 @@ import ExtendedHead from "@/components/ExtendedHead";
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import Container from "@/components/Container";
+import { useState } from "react";
 
 export default function WhackAPuff() {
+	const [timerSecs, setTimerSecs] = useState(0);
+	const [gameBegin, setGameBegin] = useState(false);
+	const [clicks, setClicks] = useState(1);
+
+	const timeLimitSecs = 60;
+
+	const startGame = () => {
+		setGameBegin(true);
+	};
+
+	const incrementCount = () => {
+		setClicks(clicks + 1);
+		console.log("clicks", clicks);
+	};
+
 	return (
 		<>
 			<Header>
@@ -12,9 +28,16 @@ export default function WhackAPuff() {
 			</Header>
 			<ExtendedHead title={"Home"} />
 			<Container>
-				<h1>Whack-A-Puff</h1>
-				<p>Whack the puff</p>
-				<Jigglypuff />
+				{!gameBegin && (
+					<button
+						type="button"
+						className="focus:outline-none text-white bg-pink-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+						onClick={startGame}
+					>
+						Start game
+					</button>
+				)}
+				{gameBegin && <Jigglypuff onClick={incrementCount} />}
 			</Container>
 		</>
 	);
