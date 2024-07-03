6a74dcae23e385cd23ca86b13fa4659232fb8ae0
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-07-02 22:24:51 -0700

    reformat codebase

--- src/hooks/useOnOutsideClick.ts
+++ src/hooks/useOnOutsideClick.ts
@@ -3,26 +3,25 @@
 import { MutableRefObject, useEffect, useRef } from "react";
 
 const useClickOutside = (
-	elementRefs: MutableRefObject<HTMLElement | null>[],
-	callback: (event: Event) => void
+  elementRefs: MutableRefObject<HTMLElement | null>[],
+  callback: (event: Event) => void
 ): void => {
-	const callbackRef = useRef(callback);
-	callbackRef.current = callback;
-	useEffect(() => {
-		const handleClickOutside = (event: Event): void => {
-			if (
-				elementRefs.every(
-					(elementRef) =>
-						!elementRef.current?.contains(event.target as Node)
-				)
-			) {
-				callbackRef.current?.(event);
-			}
-		};
-		document.addEventListener("click", handleClickOutside, true);
-		return () =>
-			document.removeEventListener("click", handleClickOutside, true);
-	}, [elementRefs]);
+  const callbackRef = useRef(callback);
+  callbackRef.current = callback;
+  useEffect(() => {
+    const handleClickOutside = (event: Event): void => {
+      if (
+        elementRefs.every(
+          (elementRef) => !elementRef.current?.contains(event.target as Node)
+        )
+      ) {
+        callbackRef.current?.(event);
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () =>
+      document.removeEventListener("click", handleClickOutside, true);
+  }, [elementRefs]);
 };
 
 export default useClickOutside;
--- src/internal/pokeapi/pokemon.ts
+++ src/internal/pokeapi/pokemon.ts
@@ -7,6 +7,6 @@ export const pokeAPIUrl = "https://pokeapi.co/api/v2";
 export const pokemonEndpoint = "pokemon";
 
 export const getPokemon = async (apiUrl: string, pokemon: string) => {
-	const res = await fetch(`${apiUrl}/${pokemonEndpoint}/${pokemon}`);
-	return res.json();
+  const res = await fetch(`${apiUrl}/${pokemonEndpoint}/${pokemon}`);
+  return res.json();
 };
